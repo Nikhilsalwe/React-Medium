d9c11bcd0fc9894bddb6aea9e27493351315fe42
Nikhil Salwe <csalwen@onjci.com>
2019-02-15 22:31:28 +0530

    props and state with setState method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    html: "HTML5",
+    javascript: "javaScript",
+    typescript: 'ES6 Basic',
+    node: 'NPM',
+    contentChangeText: 'You will see this content is going to change in 3 Sec'
+  }
+
+  
+
   render() {
+    setTimeout(() => {
+      this.setState({
+        contentChangeText: 'Ahha Content change using this.setState({}) method'
+      })
+    }, 3000);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="">
+        <h1>We get data from parent component in child in this.props as shown below</h1>
+        <h3>city name:- {this.props.city}</h3>
+        <code>this.props.city</code>
+        <hr></hr>
+        <h3>Country Name:- {this.props.country}</h3>
+        <hr></hr>
+        <h1>As describe state is nothing but javaScript object.</h1>
+        <h2>Following are the things you should learn before react:</h2>
+        <ul>
+          <li>{this.state.html}</li>
+          <li>{this.state.javascript}</li>
+          <li>{this.state.typescript}</li>
+          <li>{this.state.node}</li>
+          <li className="App-change">{this.state.contentChangeText}</li>
+        </ul>
       </div>
     );
   }
